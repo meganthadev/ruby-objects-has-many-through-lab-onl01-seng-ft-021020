2433967e58c907e86131d1f824f4162b6ef7a1f7
Megan Horsfield <meganthadev@gmail.com>
2020-05-15 06:29:22 +0000

    Automatically backed up by Learn

---
+++ lib/genre.rb
@@ -0,0 +1,20 @@
+class Genre
+  
+  attr_accessor :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end   
+
+  def self.all
+    @@all
+  end   
+
+  def songs 
+    Song.all.select do |song| song.genre == self
+   end 
+  end   
+
+end   
