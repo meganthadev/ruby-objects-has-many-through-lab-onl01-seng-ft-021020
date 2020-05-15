8adcec9775d66f518245bbd0e4b43e751954cf92
Megan Horsfield <meganthadev@gmail.com>
2020-05-15 06:24:11 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist 
+
+  attr_accessor :name, :genre
+  @@all = []
+
+  def initialize (name)
+    @name = name 
+    @@all << self
+  end 
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    self.songs.last
+  end 
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end   
+
+  def genres
+    Song.all.map {|song| song.genre}
+  end     
+
+  def self.all 
+    @@all 
+  end   
+
+end   
