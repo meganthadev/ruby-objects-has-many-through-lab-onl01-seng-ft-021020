ac724c795b91f1091e756934491af6459484c441
Megan Horsfield <meganthadev@gmail.com>
2020-05-15 06:24:15 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,28 @@
-class Artist 
+class Artist
 
   attr_accessor :name, :genre
   @@all = []
 
   def initialize (name)
-    @name = name 
+    @name = name
     @@all << self
-  end 
+  end
 
   def new_song(name, genre)
     song = Song.new(name, self, genre)
     self.songs.last
-  end 
+  end
 
   def songs
     Song.all.select {|song| song.artist == self}
-  end   
+  end
 
   def genres
     Song.all.map {|song| song.genre}
-  end     
+  end
 
-  def self.all 
-    @@all 
-  end   
+  def self.all
+    @@all
+  end
 
 end   
